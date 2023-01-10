cb1068a3129ad36e7abbcbfa95dde00e54547a35
alexc0re <nemshilov90@gmail.com>
2023-01-10 22:26:01 +0200

    added logging for all base methods

--- base_object/base.py
+++ base_object/base.py
@@ -19,15 +19,19 @@ class BaseObject:
         return self.wait.until(ec.visibility_of_element_located(locator))
 
     def is_clickable(self, locator):
+        self.log.info(f'Element {locator}  is clickable')
         return self.wait.until(ec.element_to_be_clickable(locator))
 
     def to_click(self, locator):
+        self.log.info(f'Element {locator}   clicked')
         self.is_visible(locator).click()
 
     def to_send_keys(self, locator, keys):
+        self.log.info(f'For {locator}  sended: {keys}')
         self.is_visible(locator).send_keys(keys)
 
     def get_text(self, locator):
+        self.log.info(f'From {locator}  taken text')
         return self.is_visible(locator).text
 
     def get_itemname(self, locator):
