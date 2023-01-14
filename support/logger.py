eb82021eef24b8e8ab50cf6cdc5efc2e527c326d
alexc0re <nemshilov90@gmail.com>
2023-01-14 04:57:37 +0200

    udpated logger

--- support/logger.py
+++ support/logger.py
@@ -6,7 +6,7 @@ def log_method(logLevel=log.INFO):
     logger_name = inspect.stack()[1][3]
     logger = log.getLogger(logger_name)
     logger.setLevel(logLevel)
-    fh = log.FileHandler('logfile.log')
+    fh = log.FileHandler('logfile.log', mode='w')
     formatter = log.Formatter(f"******* %(asctime)s - %(levelname)s - %(message)s", '%m/%d/%Y %I: %M: %S %p')
     fh.setFormatter(formatter)
     logger.addHandler(fh)
