2463f7f424535fad910aa4de4e6903d8ca09f6ed
alexc0re <nemshilov90@gmail.com>
2023-01-19 21:03:55 +0200

    added password json

--- pages/index_page.py
+++ pages/index_page.py
@@ -1,20 +1,39 @@
 from base_object.base import BaseObject
 from base_object.locators import Locators
 from support.asserton import Assertion
+import os
+import json
+from pathlib import Path
+
+
+FULL_PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = Path(FULL_PATH).parent
+#PATH = os.path.dirname(FULL_PATH)
+cred_path = str(PATH)+'/support/passwords.json'
+creds_file = open(cred_path, 'rb')
+creds = json.load(creds_file)
+
+USERNAME = creds['correct_username']
+PASSWORD = creds['correct_password']
+
 
 class IndexPage(BaseObject, Assertion):
+
+
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
 
     def fill_login_field(self):
-        self.to_send_keys(Locators.USERNAME, 'standard_user')
+        global USERNAME
+        self.to_send_keys(Locators.USERNAME, USERNAME)
 
     def fill_login_field_invalid_credentials(self):
         self.to_send_keys(Locators.USERNAME, 'user')
 
     def fill_password_field(self):
-        self.to_send_keys(Locators.PASSWORD, 'secret_sauce')
+        global PASSWORD
+        self.to_send_keys(Locators.PASSWORD, PASSWORD)
 
     def click_login_button(self):
         self.to_click(Locators.LOGIN_BUTTON)
